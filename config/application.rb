11707037417e065a4dec71875e1cf8ad9c2d37ce
EduRomoGO <eduromogo@outlook.com>
2014-12-18 19:59:42 +0100

    add images to precompile list

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Platecom
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.assets.precompile+= %w(*.png *.jpg *.jpeg *.gif)
   end
 end
